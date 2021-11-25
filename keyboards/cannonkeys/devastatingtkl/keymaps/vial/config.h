8db0b4450a5f0197e11740eabe6224f41e9a84d1
Peng Shao <shallpion@gmail.com>
2021-11-24 22:03:14 -0800

    Initial setup of vial for devastatingtkl

---
+++ keyboards/cannonkeys/devastatingtkl/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3B, 0x24, 0x58, 0xB3, 0x39, 0xCF, 0xFC, 0x76}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
